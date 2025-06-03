4879f4978417ae67c43e0a3824673c3109365a2f
ean <52894214+ean161@users.noreply.github.com>
2025-06-03 23:59:12 +0700

    first test

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import {
+    SafeAreaView
+} from "react-native";
+import {
+    View,
+    Text
+} from "react-native-ui-lib";
+
+export default function App() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Its working</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
